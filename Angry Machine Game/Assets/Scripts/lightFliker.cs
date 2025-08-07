05324180bdbb39f27f98a39b33f1bdd585873efa
Marcelo lima <ml0046179@gmail.com>
2025-08-07 11:41:36 +0300

    implementing sounds

--- Angry Machine Game/Assets/Scripts/lightFliker.cs
+++ Angry Machine Game/Assets/Scripts/lightFliker.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class LightFlickerToggle : MonoBehaviour
 {
     public Light flickerLight;
+    public AudioSource flickerSound; // Add this line
     private float timer;
 
     void Start()
@@ -19,14 +20,16 @@ public class LightFlickerToggle : MonoBehaviour
         if (timer <= 0f)
         {
             flickerLight.enabled = !flickerLight.enabled;  // Toggle light on/off
+
+            if (flickerSound != null)
+                flickerSound.Play(); // Play sound
+
             timer = Random.Range(0.5f, 2f);
 
             if (flickerLight.enabled)
             {
                 flickerLight.intensity = Random.Range(0.1f, 2f);  // Random intensity when light is turned on
             }
-
         }
     }
-
 }
