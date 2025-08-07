f173ef8b8ec61316dafcf4a2d1d027df376dbff3
Marcelo lima <ml0046179@gmail.com>
2025-08-07 11:28:01 +0300

    light fliker

---
+++ Angry Machine Game/Assets/Scripts/lightFliker.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class LightFlickerToggle : MonoBehaviour
+{
+    public Light flickerLight;
+    private float timer;
+
+    void Start()
+    {
+        if (flickerLight == null)
+            flickerLight = GetComponent<Light>();
+
+        timer = Random.Range(0.5f, 2f);
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0f)
+        {
+            flickerLight.enabled = !flickerLight.enabled;  // Toggle light on/off
+            timer = Random.Range(0.5f, 2f);
+
+            if (flickerLight.enabled)
+            {
+                flickerLight.intensity = Random.Range(0.1f, 2f);  // Random intensity when light is turned on
+            }
+
+        }
+    }
+
+}
