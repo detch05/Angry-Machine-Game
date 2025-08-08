61239c57d04b3ef4bc25d8f273e20f4872ec0553
mi-aw <your-username@users.noreply.github.com>
2025-08-08 08:03:54 +0100

    musics

--- Angry Machine Game/Assets/Mateusz/Authors.cs
+++ Angry Machine Game/Assets/Mateusz/Authors.cs
@@ -1,9 +1,19 @@
-
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class Authors : MonoBehaviour
 {
+    private AudioSource audioSource;
 
+    void Start()
+    {
+        // Find the AudioSource component on the BackgroundMusic GameObject
+        audioSource = FindObjectOfType<AudioSource>();
+        if (audioSource != null)
+        {
+            audioSource.Play(); // Start playing the music
+        }
+    }
 
     public void GotoMainMenu()
     {
--- Angry Machine Game/Assets/Mateusz/MainMenu.cs
+++ Angry Machine Game/Assets/Mateusz/MainMenu.cs
@@ -1,20 +1,32 @@
-using NUnit.Framework.Internal.Execution;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        // Find the AudioSource component on the BackgroundMusic GameObject
+        audioSource = FindObjectOfType<AudioSource>();
+        if (audioSource != null)
+        {
+            audioSource.Play(); // Start playing the music
+        }
+    }
 
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
     }
+
     public void GotoAuthors()
     {
         SceneManager.LoadSceneAsync(2);
     }
+
     public void ExitGame()
     {
-        Application.Quit(); 
+        Application.Quit();
     }
 }
